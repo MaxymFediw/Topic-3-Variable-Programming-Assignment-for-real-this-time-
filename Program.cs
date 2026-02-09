428ea5e1086db818b48de6ffe0d2debf4f58d34e
Maxym Fediw <362706764@GoTVDSB.ca>
2026-02-09 12:59:56 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_3_Variable_Programming_Assignment__for_real_this_time_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
